4e888d4b257aa5c724e0e5a386d8ff97f0c49dfc
Binh Ngo <bnngo97@gmail.com>
2024-02-13 14:08:15 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Binh Ngo on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
